fb48af4459c6edc06d0160c44117fffde97fac0f
sookkiong <solnaamu98@gmail.com>
2022-11-26 18:29:31 +0900

    feat: 채용페이지 title 제작

---
+++ src/components/recruit/recruit.js
@@ -0,0 +1,38 @@
+export const card = [
+  {
+    category: "[NHN][게임]",
+    content: (
+      <span>
+        모바일 게임
+        <br />
+        개발/기획/아트
+      </span>
+    ),
+    detail: "게임제작 | 경력 | 상시모집",
+    bg: "/img/cardbg1.png",
+  },
+  {
+    category: "[NHN][NHN Cloud]",
+    content: (
+      <span>
+        사업
+        <br />
+        기획/영업
+      </span>
+    ),
+    detail: "기술,사업 | 경력 | 공고기한까지",
+    bg: "/img/cardbg2.png",
+  },
+  {
+    category: "[NHN][NHN Cloud]",
+    content: (
+      <span>
+        네트워크
+        <br />
+        엔지니어링
+      </span>
+    ),
+    detail: "기술 | 경력 | 공고기한까지",
+    bg: "/img/cardbg3.png",
+  },
+];
--- src/pages/Recruit.jsx
+++ src/pages/Recruit.jsx
@@ -1,17 +1,49 @@
 import styled from "styled-components";
+import { card } from "../components/recruit/recruit";
 
 const Recruit = () => {
   return (
     <Container>
-      <div>
+      <Title>
         <div>
-          채용공고
-          <span>
+          <span
+            style={{
+              color: "#fff",
+              fontSize: "56px",
+              fontWeight: "500",
+              letterSpacing: "1.8px",
+            }}
+          >
+            채용공고
+          </span>
+          <span
+            style={{
+              display: "block",
+              color: "#fff",
+              letterSpacing: "1.8px",
+              paddingTop: "10px",
+            }}
+          >
             놀이가 작품이 되고, 상상이 현실이 되는 곳에서 함께 성장하세요!
           </span>
         </div>
-        <button>채용사이트 바로가기</button>
-      </div>
+
+        <GoMain>채용 사이트 바로가기</GoMain>
+      </Title>
+
+      <ul>
+        {card.map((value) => {
+          return (
+            <>
+              <Card>
+                <span>{value.category}</span>
+                <span>{value.content}</span>
+                <span>{value.detail}</span>
+              </Card>
+            </>
+          );
+        })}
+      </ul>
     </Container>
   );
 };
@@ -20,4 +52,29 @@ export default Recruit;
 
 const Container = styled.div`
   background-color: #e72f2c;
+  padding: 0 18%;
 `;
+
+const Title = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 80px 0 30px;
+  align-items: flex-end;
+`;
+
+const GoMain = styled.button`
+  padding: 0;
+  background: none;
+  border: 1px solid #fff;
+  padding: 5px 30px;
+  color: #fff;
+  font-weight: 500;
+  cursor: pointer;
+  &:hover {
+    background: #fff;
+    color: #000;
+    font-weight: 900;
+  }
+`;
+
+const Card = styled.li``;
